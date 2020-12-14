ed5708e731c270b33a9a515c786b689bf2d8a916
Emad <emad998@yahoo.com>
2020-12-13 19:42:39 -0800

    repair service worker 2

--- offlineServiceWorker.js
+++
@@ -1,8 +0,0 @@
-<script type="module">
-    import 'https://cdn.jsdelivr.net/npm/@pwabuilder/pwaupdate';
-
-    const el = document.createElement('pwa-update');
-    document.body.appendChild(el);
-
-
-</script>
